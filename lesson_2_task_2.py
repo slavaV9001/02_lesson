f1da6fda62b378ca34293309698c701952fea39d
slavaV9001 <slava_9001_91@mail.ru>
2026-01-18 22:30:54 +0300

    Add files via upload

--- lesson_2_task_2.py
+++ lesson_2_task_2.py
@@ -1,10 +1,7 @@
 def is_year_leap(year):
-    if year % 4 == 0:
-        return True
-    else:
-        return False
+    return (year % 4 == 0)
 
+year = int(input("Введите год: "))
+result = is_year_leap(year)
 
-num = int(input("Введите год: "))
-result = is_year_leap(num)
-print(f"год {num} делится на 4: {result}")
+print(f"Год {year} високосный: {result}")
